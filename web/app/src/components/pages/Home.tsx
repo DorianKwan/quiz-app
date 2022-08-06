01ed1871eb2784e303a421c067c7e852c9d85c1b
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-08-06 11:29:18 -0600

    feat: fetch questions list on home mount

--- web/app/src/components/pages/Home.tsx
+++ web/app/src/components/pages/Home.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
-import { PageWrapper } from '../utility';
+import { ApiData } from 'quiz-shared';
+import { useAsyncEffect } from 'src/hooks';
+import { coreServiceApi } from 'src/api';
+import { Loader, PageWrapper } from '../utility';
 
 export const Home = () => {
+  const [quizQuestions, setQuizQuestions] = useState<ApiData.QuestionsList>([]);
+  const [isLoadingQuestions, setIsLoadingQuestions] = useState(true);
+
+  useAsyncEffect(async () => {
+    const questions = await coreServiceApi.questions.getQuestionsList();
+    setQuizQuestions(questions);
+    setIsLoadingQuestions(false);
+  }, []);
+
+  if (isLoadingQuestions) {
+    return <Loader />;
+  }
+
   return (
     <HomePageWrapper>
       <h1>Quiz</h1>
