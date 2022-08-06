0a0d474a24d5a6dfd4fcb585700035a0cc1e898f
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-08-06 00:50:29 -0600

    feat: create question service method determine quiz results

--- services/core-service/src/routes/questions/service.ts
+++ services/core-service/src/routes/questions/service.ts
@@ -16,8 +16,76 @@ export default class QuestionService {
       });
     }
   }
+
+  async determineQuizResults(
+    answers: { questionId: number; answer: string }[],
+  ) {
+    try {
+      const quizAnswers = answers.reduce(
+        (acc: { [questionId: number]: string }, { questionId, answer }) => {
+          acc[questionId] = answer;
+          return acc;
+        },
+        {},
+      );
+
+      const questions = await this.questionRepo.getQuestions();
+
+      questions.forEach(({ id, question, options }) => {
+        const questionAnswer: string | undefined = quizAnswers[id];
+
+        const doesAnswerExistForQuestion = questionAnswer;
+
+        if (doesAnswerExistForQuestion) {
+          throw new ServiceError({
+            type: QuestionServiceError.MissingQuestionAnswer,
+            message: 'An answer is missing for a quiz question',
+            data: {
+              id,
+              question,
+              options,
+            },
+          });
+        }
+
+        const isAnswerOptions = options.includes(questionAnswer);
+
+        if (!isAnswerOptions) {
+          throw new ServiceError({
+            type: QuestionServiceError.AnswerNotFound,
+            message: `Answer for question ${id} was not found`,
+            data: {
+              id,
+              question,
+              answerGiven: questionAnswer,
+            },
+          });
+        }
+      });
+
+      const questionAnswers = await this.questionRepo.getQuestionAnswers();
+
+      const quizResults = questionAnswers.map(
+        ({ question_id: questionId, answer }) => {
+          const quizAnswer = quizAnswers[questionId];
+          const isCorrect = quizAnswer === answer;
+
+          return { questionId, isCorrect };
+        },
+      );
+
+      return quizResults;
+    } catch (e: any) {
+      throw new ServiceError({
+        type: QuestionServiceError.FileReadError,
+        message: 'Something went wrong while reading questions or answers file',
+      });
+    }
+  }
 }
 
 export enum QuestionServiceError {
   FileReadError = 'question/file-read',
+  AnswerNotFound = 'question/answer-not-found',
+  MissingQuestionAnswer = 'question/missing-answer',
 }
