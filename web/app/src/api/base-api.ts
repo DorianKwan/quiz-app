0f2fe4297283daf2e06afbdbf0a77c86bbaf0d43
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-08-06 11:29:08 -0600

    fix: base api post request typing

--- web/app/src/api/base-api.ts
+++ web/app/src/api/base-api.ts
@@ -7,7 +7,11 @@ export const createBaseApi = (baseUrl: string) => {
     get: async <T>(url: string, config?: AxiosRequestConfig): Promise<T> => {
       return axios.get(`${baseApiUrl}${url}`, config);
     },
-    post: async <T>(url: string, body: any, config?: AxiosRequestConfig) => {
+    post: async <T>(
+      url: string,
+      body: any,
+      config?: AxiosRequestConfig,
+    ): Promise<T> => {
       return axios.post(`${baseApiUrl}${url}`, body, config);
     },
   };
