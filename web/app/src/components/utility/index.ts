5c6dfa366f6373e19f350a3674a57e54831768ce
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-08-06 12:40:38 -0600

    feat: export new util components from util index

--- web/app/src/components/utility/index.ts
+++ web/app/src/components/utility/index.ts
@@ -1,2 +1,5 @@
+export { AnimatedText } from './AnimatedText';
+export { GradientContainer } from './GradientContainer';
+export { FadeIn } from './FadeIn';
 export { PageWrapper } from './PageWrapper';
 export { Loader } from './Loader';
