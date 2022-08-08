06ae807e9e724f71d87f6ef2bf4b13543f200151
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-08-07 18:18:03 -0600

    refactor: gradient container; add padding & remove border radius

--- web/app/src/components/utility/GradientContainer.tsx
+++ web/app/src/components/utility/GradientContainer.tsx
@@ -5,9 +5,13 @@ export const GradientContainer = styled.div<{ background: Gradient }>`
   position: relative;
   width: 100%;
   height: 100%;
-  border-radius: 1.75rem;
   transition: background 0.3s linear;
   color: white;
   background: ${({ background }) => background.fallback};
   background: ${({ background }) => background.background};
+  padding: 1rem;
+
+  @media only screen and (min-width: 420px) {
+    padding: 2rem;
+  }
 `;
