8ba0c3ea69e5d5fc231d4549efbd1382c15d493b
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-08-07 22:14:01 -0600

    feat: add post quiz answers method to FE api

--- web/app/src/api/api.ts
+++ web/app/src/api/api.ts
@@ -17,7 +17,20 @@ const createApi = (baseUrl: string) => {
       async getQuestionsList() {
         type Response = AxiosResponse<ApiResponse<ApiData.QuestionsList>>;
 
-        const response = await coreServiceApi.get<Response>('/questions');
+        const response = await coreServiceApi.get<Response>('/quiz');
+
+        if (response.status !== 200) throw new Error(response.statusText);
+
+        const { data } = response.data;
+
+        return data;
+      },
+      async postQuizAnswers(quizAnswers: ApiData.Answer[]) {
+        type Response = AxiosResponse<ApiResponse<ApiData.QuizResults>>;
+
+        const response = await coreServiceApi.post<Response>('/quiz', {
+          answers: quizAnswers,
+        });
 
         if (response.status !== 200) throw new Error(response.statusText);
 
