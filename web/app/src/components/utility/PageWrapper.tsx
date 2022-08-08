5d9e97a7691292bdac7279d6b179593e2efb7825
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-08-07 18:18:21 -0600

    refactor: page wrapper component; remove padding

--- web/app/src/components/utility/PageWrapper.tsx
+++ web/app/src/components/utility/PageWrapper.tsx
@@ -1,11 +1,8 @@
 import styled from '@emotion/styled';
 
 export const PageWrapper = styled.main`
+  display: grid;
+  place-items: center;
   width: 100%;
   height: 100%;
-  padding: 1.25rem;
-
-  @media screen and (min-width: 640px) {
-    padding: 3rem;
-  }
 `;
