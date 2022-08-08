a70865325b364a6014e259dd6ef23683b86be1dd
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-08-07 18:14:18 -0600

    feat: add question and answer to api data

--- lib/shared/src/api-types.ts
+++ lib/shared/src/api-types.ts
@@ -1,7 +1,19 @@
 export namespace ApiData {
-  export type QuestionsList = {
+  export type Question = {
     id: number;
     question: string;
     options: string[];
+  };
+
+  export type QuestionsList = ApiData.Question[];
+
+  export type Answer = {
+    questionId: number;
+    answer: string;
+  };
+
+  export type QuizResults = {
+    questionId: number;
+    isCorrect: boolean;
   }[];
 }
