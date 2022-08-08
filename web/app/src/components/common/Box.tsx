417356a937dbaa9285b5c312ace63986119aab48
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-08-07 18:15:50 -0600

    feat: create common box component

---
+++ web/app/src/components/common/Box.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const Box = styled.div`
+  position: relative;
+  padding: 2rem;
+  width: clamp(15rem, 100%, 40rem);
+  height: clamp(10rem, 100%, 25rem);
+  border-radius: 1.75rem;
+  background: white;
+`;
