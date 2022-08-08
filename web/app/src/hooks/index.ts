68edecf63d5b7684621019d9c1e78fe3a325d5c0
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-08-07 22:14:37 -0600

    feat: create use quiz custom hook

--- web/app/src/hooks/index.ts
+++ web/app/src/hooks/index.ts
@@ -1,2 +1,4 @@
-export { useTypedTheme } from './use-typed-theme';
 export { useAsyncEffect } from './use-async-effect';
+export { useRandomGradient } from './use-random-gradient';
+export { useTypedTheme } from './use-typed-theme';
+export { useQuiz } from './use-quiz';
